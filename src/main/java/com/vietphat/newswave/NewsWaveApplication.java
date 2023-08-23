518c5ce7b7d8de853b571b6e8dd592f0bff90c00
PHAT\vietp <vietphatt1909@gmail.com>
2023-08-23 08:11:54 +0700

    first commit

---
+++ src/main/java/com/vietphat/newswave/NewsWaveApplication.java
@@ -0,0 +1,13 @@
+package com.vietphat.newswave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsWaveApplication.class, args);
+	}
+
+}
